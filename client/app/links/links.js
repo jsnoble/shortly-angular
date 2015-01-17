157115cca200dea41fb3c48bf26593b536979db8
Jared Noble <jsnoble1@gmail.com>
2015-01-17 15:52:41 -0700

    basic reqs done

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -8,13 +8,11 @@ angular.module('shortly.links', [])
       Links.getLinks()
         .then(function(data){
           $scope.data.links = data;
-          console.log('this is scope: ', $scope.data.links);
         })
         .catch(function(err){
           console.error(err);
         })
     };
     $scope.getLinks();
-    console.log('links data is : '+ JSON.stringify($scope.data));
-    console.log('this is scope: ', $scope.data.links);
+
 });
